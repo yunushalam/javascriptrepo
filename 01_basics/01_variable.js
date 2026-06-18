ad135f3ffda1ae2b56889f1220cba116c4d5dcf1
yunushalam <yunushalamalam32@gmail.com>
2026-06-18 16:05:12 +0000

    datatypes,variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountid=12345;
+let accountEmail="yunushalam21@gmail.com";
+var accountPassword="1234";
+
+//accountid=234;
+
+//const datatypes value is not to be changed.
+
+
+/* var is not used in javascript due to issue 
+in block scope and functional scope.*/ 
+
+accountEmail="yunushalam";
+accountPassword="yunush@alam";
+
+
+console.log(accountid);
+console.log([accountid,accountPassword,accountEmail]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3); we are using nodejs so alert is not defined
+ 
+
+console.log(3+3);  //code rediblity that should be high
+
+let name="yunus";
+let age=21;
+let isLoggedIn=false;   
+
+//number => 2 to power 53-1
+//string =""
+//boolean => true, false
+//undefined => not defined variable
+//null => standlone value that represents nothing or empty value
+//bigint => that represents integer value greater than 2 to power 53-1
+//symbol => that represents unique value
+// object => that represents collection of data
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
