dd0d80776c5c97f51791cc9e355e2f11cb34dfa8
yunushalam <yunushalamalam32@gmail.com>
2026-06-20 10:28:41 +0000

    message

--- 01_basics/08_MathNumber.js
+++ 01_basics/08_MathNumber.js
@@ -1,7 +1,7 @@
     const score =400;
    // console.log(score);
     const balance=new Number(100);
-    //console.log(balance);
+    console.log(balance);
     
    
    // console.log(balance.toString().length);
@@ -25,10 +25,10 @@
    // console.log((Math.random()*10) +1 );
    // console.log(Math.floor(Math.random()*10) + 1 );
 
-    const min = 10;
-    const max = 20;
+   //const min = 10;
+   // const max = 20;
 
-   console.log (Math.floor(Math.random() *(max-min+1) *min)
+   //console.log (Math.floor(Math.random() *(max-min+1) *min)
     
 
 
