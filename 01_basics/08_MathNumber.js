cfda33fe992f2e4f84665e201f90694ebf377c41
yunushalam <yunushalamalam32@gmail.com>
2026-06-20 09:40:02 +0000

    mathnumber

--- 01_basics/08_MathNumber.js
+++ 01_basics/08_MathNumber.js
@@ -3,7 +3,7 @@
     const balance=new Number(100);
     //console.log(balance);
     
-
+   
    // console.log(balance.toString().length);
     //console.log(balance.toFixed(1));
     const otherNumber=1123.2343;
