c5aa4e15403adb6b6f9896b1c672597778117f6a
yunushalam <yunushalamalam32@gmail.com>
2026-06-22 14:51:38 +0000

    date and time

---
+++ 01_basics/09_data_and_Time.js
@@ -0,0 +1,10 @@
+let myDate=new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+let myCreatedDate=new Date(2026,6,22);
+//console.log(myCreatedDate.toDateString());
+
+
