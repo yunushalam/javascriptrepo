1566b2a92e605a5ec15f1508e1e635fc2060f8be
yunushalam <yunushalamalam32@gmail.com>
2026-06-23 07:20:22 +0000

    object

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -1,6 +1,21 @@
-//
 // object literals
-
-const object={
-    name:"yunush"
+//const mySym=Symbol("key1");
+const jsUser={
+    name:"yunush",
+    last_name:"alam",
+   [mySym]:"mykey1",
+    email:"yunushalam@gmail.com",
+    isLoggedIn="yes"
 }
+console.log(jsUser.email);
+console.log(jsUser[mySym]);
+
+jsUser.email="yunushalam21@hmail.com"
+
+jsUser.greeting=function () {
+    console.log(`Hello js user,${this.name}`);
+}
+
+console.log(jsUser.greeting());
+
+
