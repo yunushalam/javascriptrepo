9e0ed2b52f6ebd3a041805acb982b9b7d50f5807
yunushalam <yunushalamalam32@gmail.com>
2026-06-23 05:56:28 +0000

    Array

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,41 @@
+let Array=[2,3,4];
+console.log(Array);
+console.log(Array[0]);
+
+const myArray=["yunush","alam"];
+console.log(myArray);
+
+const myArray=new Array(1.34,45);
+console.log(myArray);
+
+//______Array methods____________
+
+//myArray.push(9)
+//myArray.push(2)
+//myArray.pop()
+
+//myArray.unshift(9);
+//myArray.shift()
+
+console.log(myArray.includes());
+console.log(myArray.indexOf(9));
+
+
+
+console.log(myArray);
+
+//slice,splice
+
+console.log("a",myArray);
+const myn1=myArray.slice(1,3);
+console.log(myn1);
+console.log("B",myArray)
+
+const myn2=myArray.splice(1,3);
+console.log(myn2);
+
+
+
+
+
+
