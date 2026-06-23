4877fdb037e0788d9b06583bd63117ed9470588f
yunushalam <yunushalamalam32@gmail.com>
2026-06-23 04:25:45 +0000

    dataandtime

--- 01_basics/09_data_and_Time.js
+++ 01_basics/09_data_and_Time.js
@@ -6,5 +6,22 @@ let myDate=new Date()
 
 let myCreatedDate=new Date(2026,6,22);
 //console.log(myCreatedDate.toDateString());
+let myTmeStamp=Date.now();
 
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate=myDate();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+//`${newDate.getDay()}and the time`
+
+newDate.toLocaleString('default' ,
+    {
+        weekday: "long"
+    }
+)
 
