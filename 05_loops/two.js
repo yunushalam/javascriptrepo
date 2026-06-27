17697795728e7c3a84bc8dc44e6a32fe93fb9b76
yunushalam <yunushalamalam32@gmail.com>
2026-06-27 12:00:59 +0000

    loops

---
+++ 05_loops/two.js
@@ -0,0 +1,25 @@
+
+    //while
+
+    let i=1;
+    while(i<10){
+        console.log(i);
+        i++;
+    }
+
+    // do while loop
+    //do while loop execute 
+    // atlist one time statement 
+    // whenever it is wrong
+    let j=1;
+    do{
+        console.log(j);
+        
+    }
+    while(j>=10)
+
+        let arr = 0
+    while (arr < myArray.length) {
+        //console.log(`Value is ${myArray[arr]}`);
+        arr = arr + 1
+    }
