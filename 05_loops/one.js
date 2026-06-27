cd1d827bd2c5440148cc5062ef0d642663cd6e98
yunushalam <yunushalamalam32@gmail.com>
2026-06-27 08:48:00 +0000

    loops

---
+++ 05_loops/one.js
@@ -0,0 +1,49 @@
+// for loop
+
+let n=10;
+for(let i=1;i<n;i++){
+   //console.log(i);
+}
+//nested loop
+
+let m=10;
+for(let i=1;i<=m;i++){
+   console.log("outer loop:"+i);
+   for (let j = 0; j <=m; j++) {
+     // console.log("inner loop:"+j);
+      
+   }
+}
+
+let myarray=["flash","batman","superhero"]
+for (let index = 0; index < myarray.length; index++) {
+   const element = myarray[index];
+   //console.log(element
+
+   
+   
+   
+}
+
+//break and countinue
+
+
+// countinue
+let o=10;
+for(let i=1;i<o;i++){
+   if(i==5){
+      continue;
+   }
+     console.log(i);
+}
+
+
+//break statement
+let p=10;
+for(let i=1;i<p;i++){
+   if(i==5){
+      break;
+   }
+     console.log(i);
+}
+
